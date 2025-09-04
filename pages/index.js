9c501db51c6d941ef15b6a43c2d96c716e68dd62
horvathgabor8787 <horvathgabor8787@gmail.com>
2025-09-04 15:17:49 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-pink-100 to-purple-200">
-      <h1 className="text-4xl font-bold text-center">Baby Panda Landing működik 🎉</h1>
-    </div>
-  );
+  const router = useRouter();
+  useEffect(() => {
+    router.replace("/nevre-szolo-babaajandek");
+  }, [router]);
+  return null;
 }
