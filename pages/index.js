348963892e3f34ab578e89fe8e8afdff9e543322
horvathgabor8787 <horvathgabor8787@gmail.com>
2025-09-04 15:00:08 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-pink-100 to-purple-200">
+      <h1 className="text-4xl font-bold text-center">Baby Panda Landing működik 🎉</h1>
+    </div>
+  );
+}
